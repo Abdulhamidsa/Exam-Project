ae09bc0d254d82b34c0ba0646b9934759256d954
Jonas Holbech <abdu3793@stud.kea.dk>
2022-06-02 14:43:27 +0200

    local

--- js/checkout-first.js
+++ js/checkout-first.js
@@ -2,22 +2,6 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
-/*   document.querySelector.(".section-frist-cheackout button:last-child")
- */
-
-/* document.getElementById("circle1").addEventListener("click", progressLine1());
-document.getElementById("circle2").addEventListener("click", progressLine2());
-
-function progressLine1() {
-  document.querySelector("#circle1").style.backgroundColor = "blue";
-}
-function progressLine2() {}
- */
-
-/* const circle1 = document.getElementById("next");
-circle1.addEventListener("click", function onClick(event) {
-  document.querySelector("#circle1").style.backgroundColor = "#b0b0b0";
-}); */
 const step1method1 = document.getElementById("next-button-cheackout1");
 step1method1.addEventListener("click", function onClick(event) {
   document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
@@ -76,3 +60,39 @@ function displayMore() {
     text.style.opacity = "0";
   }
 } */
+const step1option1 = document.getElementById("next-button-cheackout2");
+step1option1.addEventListener("click", function onClick(event) {
+  document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
+  document.querySelector(".line1").style.backgroundColor = "var(--brown)";
+  /*   document.querySelector(".extra-line").style.visibility = "visible";
+   */
+});
+const step1option2 = document.getElementById("next-button-cheackout2-method2");
+step1option2.addEventListener("click", function onClick(event) {
+  document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
+  document.querySelector(".line1").style.backgroundColor = "var(--brown)";
+});
+const step2option2 = document.getElementById("prev-button-delivery2");
+step2option2.addEventListener("click", function onClick(event) {
+  document.querySelector("#circle2").style.backgroundColor = "#b0b0b0";
+  document.querySelector(".line1").style.backgroundColor = "#b0b0b0";
+});
+
+const step3option2 = document.getElementById("next-button-payment2");
+step3option2.addEventListener("click", function onClick(event) {
+  document.querySelector(".line2").style.backgroundColor = "var(--brown)";
+  document.querySelector("#circle3").style.backgroundColor = "var(--brown)";
+});
+const step4option2 = document.getElementById("prev-button-summary2");
+step4option2.addEventListener("click", function onClick(event) {
+  document.querySelector(".line2").style.backgroundColor = "#b0b0b0";
+  document.querySelector("#circle3").style.backgroundColor = "#b0b0b0";
+});
+
+/* const step5option2 = document.getElementById("next-button-finish-pay");
+step5option2.addEventListener("click", function onClick(event) {
+  document.querySelector(".line3").style.backgroundColor = "var(--brown)";
+  document.querySelector("#circle4").style.backgroundColor = "var(--brown)";
+  document.querySelector(".slider-checkout").style.display = "none";
+});
+ */
