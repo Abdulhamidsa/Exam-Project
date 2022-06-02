f8e6410fe194d79852249e3dca1fee5b9df845f0
Jonas Holbech <abdu3793@stud.kea.dk>
2022-06-02 20:11:24 +0200

    finished-checkout-function

--- js/checkout-first.js
+++ js/checkout-first.js
@@ -2,43 +2,69 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
+function changeMethod() {
+  let method = document.getElementById("method1");
+  let method2 = document.getElementById("method2");
+  method.classList.add("hidden");
+  method2.classList.remove("hidden");
+}
+function changeMethod2() {
+  let method2 = document.getElementById("method2");
+  let method1 = document.getElementById("method1");
+  method2.classList.add("hidden");
+  method1.classList.remove("hidden");
+}
 const step1method1 = document.getElementById("next-button-cheackout1");
 step1method1.addEventListener("click", function onClick(event) {
   document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
   document.querySelector(".line1").style.backgroundColor = "var(--brown)";
-  /*   document.querySelector(".extra-line").style.visibility = "visible";
-   */
 });
 const step1method2 = document.getElementById("next-button-cheackout1-method2");
 step1method2.addEventListener("click", function onClick(event) {
   document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
   document.querySelector(".line1").style.backgroundColor = "var(--brown)";
 });
-const step2 = document.getElementById("prev-button-delivery");
+/* const step2 = document.getElementById("prev-button-delivery");
 step2.addEventListener("click", function onClick(event) {
   document.querySelector("#circle2").style.backgroundColor = "#b0b0b0";
   document.querySelector(".line1").style.backgroundColor = "#b0b0b0";
-  /*   document.getElementById("checkout-step1").scrollIntoView(true);
-   */
-});
-const step3 = document.getElementById("next-button-payment");
+}); */
+const step3 = document.getElementById("next-button-summary");
 step3.addEventListener("click", function onClick(event) {
   document.querySelector(".line2").style.backgroundColor = "var(--brown)";
   document.querySelector("#circle3").style.backgroundColor = "var(--brown)";
 });
-const step4 = document.getElementById("prev-button-summary");
+const step4 = document.getElementById("prev-button-time-picking-method1");
 step4.addEventListener("click", function onClick(event) {
+  document.querySelector(".line1").style.backgroundColor = "#b0b0b0";
+  document.querySelector("#circle2").style.backgroundColor = "#b0b0b0";
+});
+const step5 = document.getElementById("prev-button-time");
+step5.addEventListener("click", function onClick(event) {
   document.querySelector(".line2").style.backgroundColor = "#b0b0b0";
   document.querySelector("#circle3").style.backgroundColor = "#b0b0b0";
 });
-
-const step5 = document.getElementById("next-button-finish-pay");
+const step6 = document.getElementById("next-button-payment");
+step6.addEventListener("click", function onClick(event) {
+  document.querySelector(".line3").style.backgroundColor = "var(--brown)";
+  document.querySelector("#circle4").style.backgroundColor = "var(--brown)";
+});
+const step7 = document.getElementById("prev-button-summary");
+step7.addEventListener("click", function onClick(event) {
+  document.querySelector(".line3").style.backgroundColor = "#b0b0b0";
+  document.querySelector("#circle4").style.backgroundColor = "#b0b0b0";
+});
+const finalstep = document.getElementById("next-button-finish-pay");
+finalstep.addEventListener("click", function onClick(event) {
+  document.querySelector(".slider-checkout").style.display = "none";
+});
+/* const step5 = document.getElementById("next-button-finish-pay");
 step5.addEventListener("click", function onClick(event) {
   document.querySelector(".line3").style.backgroundColor = "var(--brown)";
   document.querySelector("#circle4").style.backgroundColor = "var(--brown)";
   document.querySelector(".slider-checkout").style.display = "none";
 });
-
+ */
 function displayMore() {
   let x = document.getElementById("delivery-info");
   let y = document.getElementById("delivery-info2");
@@ -50,8 +76,7 @@ function displayMore() {
     x.style.display = "none";
     y.style.display = "none";
   }
-}
-
+} /*
 /* function displayMore2() {
   let text = document.getElementById("delivery-info2");
   if (text.style.opacity === "0") {
@@ -64,8 +89,6 @@ const step1option1 = document.getElementById("next-button-cheackout2");
 step1option1.addEventListener("click", function onClick(event) {
   document.querySelector("#circle2").style.backgroundColor = "var(--brown)";
   document.querySelector(".line1").style.backgroundColor = "var(--brown)";
-  /*   document.querySelector(".extra-line").style.visibility = "visible";
-   */
 });
 const step1option2 = document.getElementById("next-button-cheackout2-method2");
 step1option2.addEventListener("click", function onClick(event) {
@@ -83,16 +106,22 @@ step3option2.addEventListener("click", function onClick(event) {
   document.querySelector(".line2").style.backgroundColor = "var(--brown)";
   document.querySelector("#circle3").style.backgroundColor = "var(--brown)";
 });
-const step4option2 = document.getElementById("prev-button-summary2");
+const step4option2 = document.getElementById("prev-button-time-picker");
 step4option2.addEventListener("click", function onClick(event) {
-  document.querySelector(".line2").style.backgroundColor = "#b0b0b0";
   document.querySelector("#circle3").style.backgroundColor = "#b0b0b0";
+  document.querySelector(".line2").style.backgroundColor = "#b0b0b0";
 });
-
-/* const step5option2 = document.getElementById("next-button-finish-pay");
+const step5option2 = document.getElementById("next-button-payment-method2");
 step5option2.addEventListener("click", function onClick(event) {
   document.querySelector(".line3").style.backgroundColor = "var(--brown)";
   document.querySelector("#circle4").style.backgroundColor = "var(--brown)";
+});
+const step6option2 = document.getElementById("prev-button-summary-methode2");
+step6option2.addEventListener("click", function onClick(event) {
+  document.querySelector("#circle4").style.backgroundColor = "#b0b0b0";
+  document.querySelector(".line3").style.backgroundColor = "#b0b0b0";
+});
+const step7option2 = document.getElementById("next-button-finish-pay-method2");
+step7option2.addEventListener("click", function onClick(event) {
   document.querySelector(".slider-checkout").style.display = "none";
 });
- */
