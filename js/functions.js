4609f7ee06162d39e4bb46388f9ad8b27c1a0dbc
Jonas Holbech <abdu3793@stud.kea.dk>
2022-06-06 11:56:10 +0200

    launch-cart-page

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function openmenumobile() {
-  var x = document.getElementById("mainmenu");
-  if (x.style.display !== "inline-grid") {
-    x.style.display = "inline-grid";
-  } else {
-    x.style.display = "none";
-  }
-}
